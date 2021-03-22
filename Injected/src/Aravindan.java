c62f5a59fbd3b8be873f7845170a74ff10cfaa50
LENOVO <LENOVO@DESKTOP-B5O7AU0>
2021-03-22 09:11:06 +0530

    Coding

--- Injected/src/Aravindan.java
+++ Injected/src/Aravindan.java
@@ -2,7 +2,12 @@
 public class Aravindan {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int a=30;
+		int b = 90;
+		int sum = a+b;
+		System.out.println(sum);
+		
 
 	}
 
